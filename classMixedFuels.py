8c1057a9ea424a268c1336b0fb582c5cd2bd1d14
InspBarn <gregoire.vekemans@student.uclouvain.be>
2020-10-12 15:43:47 +0200

    Specify MixedFuels

--- classMixedFuels.py
+++ classMixedFuels.py
@@ -3,49 +3,62 @@
 import math as mt
 import numpy as np
 
-import classCombustion as cbt
+import classCombustible as cbt
 
+"""
+MixedFuels est un objet représentant un fuel le plus général possible consistant en un
+mélange de 'N' composants chimiques différents de fraction 'X' dont les paramètres sont :
+	- fuelT    : liste d'objets 'Combustible' représentant la combustion de chaque composants
+				du fuel. Notons que chaque composants ne brule pas spcialement (e.g. 'N2')
+	- fraction : np.array de floats representant la fraction de chaque composant. La somme
+				des éléments vaut 1.
+	- reactant : dictionnaire des réactifs {"comp": frac} ou :
+				* comp est le nom du composant
+				* frac est la fraction massique du composants dans les réactifs
+	- product  : dictionnaire des produits {"comp": frac} ou :
+				* comp est le nom du composant
+				* frac est la fraction massique du composants dans les produits
+"""
 class MixedFuels:
-	def __init__(self, fuel, gas, air_to_fuel_ratio):
-		self.fuel = []
-		self.fraction = np.zeros((len(fuel),))
-
-		for (index,element) in fuel.items():
-			(fuel_type, fuel_fraction) = element
-
+	def __init__(self, fuel: dict, gas: dict, AF_ratio: float):
+		fuelT, fraction = [],[]
+		for (fuel_type,fuel_fraction) in fuel.items():
 			fuel_decomposition = {'O':0,'H':0,'C':0}
 			for chem in fuel.index():
-				i = fuel_type.index(chem) +1
-				comp = 0
-				while i < len(fuel_type) or i == -1:
+				i,c = fuel_type.index(chem) +1, 0
+				while (i<len(fuel_type)) or (i==-1):
 					try:
-						comp = comp*10 + int(fuel_type[i])
+						c = c*10 + int(fuel_type[i])
 						i += 1
 					except ValueError:
 						i = -1
 				fuel_decomposition[chem] = comp
 
-			self.fuel = self.fuel.append(cbt.Combustion(
-											x=fuel_decomposition{'O'},
-											y=fuel_decomposition{'H'},
-											z=fuel_decomposition{'C'}
-										))
-			self.fraction[index] = fuel_fraction
+			fuelT.append(cbt.Combustion(
+				x=fuel_decomposition{'O'},
+				y=fuel_decomposition{'H'},
+				z=fuel_decomposition{'C'}
+			))
+			fraction.append(fuel_fraction)
 
-			self.reactants = fuel
-			
-			if 'N2' in self.fuel:
-				self.reactants['N2'] = self.fuel['N2'] + air_to_fuel_ratio*(3.76*28)/(32+3.76*28)
-			else:
-				self.reactants['N2'] = air_to_fuel_ratio*(3.76*28)/(32+3.76*28)
-			
-			if 'O2' in self.fuel:
-				self.reactants['O2'] = self.fuel['O2'] + air_to_fuel_ratio*32/(32+3.76*28)
-			else:
-				self.reactants['O2'] = air_to_fuel_ratio*32/(32+3.76*28)
+		self.fuelT    = fuelT
+		self.fraction = np.array(fraction)
 
-			glob = sum(np.array(self.reactants.values()))
-			for (chem,comp) in self.reactants:
-				self.reactants[chem] = comp / glob
+		self.reactant = fuel
+		self.product  = gas
 
-			self.products = gas
+		if 'N2' in self.fuel:
+			self.reactant['N2'] = self.fuel['N2'] + AF_ratio*(3.76*28)/(32+3.76*28)
+		else:
+			self.reactant['N2'] = AF_ratio*(3.76*28)/(32+3.76*28)
+
+		if 'O2' in self.fuel:
+			self.reactant['O2'] = self.fuel['O2'] + AF_ratio*32/(32+3.76*28)
+		else:
+			self.reactant['O2'] = AF_ratio*32/(32+3.76*28)
+
+		glob = sum(self.reactant.values())
+		for (chem,comp) in self.reactant:
+			self.reactant[chem] = comp / glob
+
+		return self
